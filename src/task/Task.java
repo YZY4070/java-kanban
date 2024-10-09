209ea414c63004b23c410b698c241c6df5a290dc
YEZZY4070 <muteallchat30@yandex.ru>
2024-10-10 02:14:59 +0300

    fix + checkStyle1

--- src/task/Task.java
+++ src/task/Task.java
@@ -21,7 +21,7 @@ public class Task {
         this.duration = duration;
     }
 
-    public Task(int id, String name, String description, Status status){
+    public Task(int id, String name, String description, Status status) {
         this.id = id;
         this.name = name;
         this.description = description;
